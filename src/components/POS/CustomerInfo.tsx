6e195067d0890e3749c54facba1018d832d93b91
irfathmostofa <irfathmostofa1@gmail.com>
2025-09-08 17:06:57 +0600

    new update

--- src/components/POS/CustomerInfo.tsx
+++ src/components/POS/CustomerInfo.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { demoCustomers } from "@/data/demoCustomers";
